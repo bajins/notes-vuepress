1fd72655694474168ac123eeb55e1c6de1281fc8
woytu <admin@woytu.com>
2018-11-29 13:42:50 +0800

    Update scp.sh

--- VPS/scp.sh
+++ VPS/scp.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-##需要手动安装expect包：yum -y install  expect  expect-devel
-## 密码设置
+## 需要手动安装expect包：yum -y install expect expect-devel
+## 远程IP的密码设置，用${des_pass}获取
 des_pass=123456
 expect -c "
-##设置拷贝的时间，超时时间-1为永不超时
+## 设置拷贝的时间，超时时间-1为永不超时
 set timeout -1
 ## 设置上传配置：文件或文件夹、IP
 spawn scp -P 965 -p -r /www/wwwroot/file root@192.168.1.1:/www/wwwroot/test
-##expect \"password:\"
-##send \"${des_pass}\r\"
+## expect \"password:\"
+## send \"${des_pass}\r\"
 expect {
   \"*yes/no*\" {send \"yes\r\"; exp_continue}
-  \"*password*\" {send \"${des_pass}\r\";} ##远程IP的密码。
+  \"*password*\" {send \"${des_pass}\r\";}
 }
 expect eof
 "
