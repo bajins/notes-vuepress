0a1d45a6b30ae4ab5f481ef2295bf1a5dbaabb62
woytu <admin@woytu.com>
2018-11-29 13:43:40 +0800

    Update scp.sh

--- VPS/scp.sh
+++ VPS/scp.sh
@@ -5,8 +5,8 @@ des_pass=123456
 expect -c "
 ## 设置拷贝的时间，超时时间-1为永不超时
 set timeout -1
-## 设置上传配置：文件或文件夹、IP
-spawn scp -P 965 -p -r /www/wwwroot/file root@192.168.1.1:/www/wwwroot/test
+## 设置上传配置：文件或文件夹、IP、端口
+spawn scp -P 223 -p -r /www/wwwroot/file root@192.168.1.1:/www/wwwroot/test
 ## expect \"password:\"
 ## send \"${des_pass}\r\"
 expect {
