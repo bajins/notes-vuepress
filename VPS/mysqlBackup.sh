827d55e63447841d3c0c2e78649d864e73339c52
woytu <admin@woytu.com>
2019-02-12 20:28:31 +0800

    Update mysqlBackup.sh

--- VPS/mysqlBackup.sh
+++ VPS/mysqlBackup.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
 
 # 以下配置信息请自己修改
-mysql_user="USER" #MySQL备份用户
-mysql_password="PASSWORD" #MySQL备份用户的密码
+
+# MySQL备份用户
+mysql_user="USER"
+# MySQL备份用户的密码
+mysql_password="PASSWORD"
+# 主机地址
 mysql_host="localhost"
+# 端口
 mysql_port="3306"
-mysql_charset="utf8" #MySQL编码
-backup_db_arr=("db1" "db2") #要备份的数据库名称，多个用空格分开隔开 如("db1" "db2" "db3")
-backup_location=/opt/mysql  #备份数据存放位置，末尾请不要带"/",此项可以保持默认，程序会自动创建文件夹
-expire_backup_delete="ON" #是否开启过期备份删除 ON为开启 OFF为关闭
-expire_days=3 #过期时间天数 默认为三天，此项只有在expire_backup_delete开启时有效
+# MySQL编码
+mysql_charset="utf8"
+# 要备份的数据库名称，多个用空格分开隔开 如("db1" "db2" "db3")
+backup_db_arr=("db1" "db2")
+# 备份数据存放位置，末尾请不要带"/",此项可以保持默认，程序会自动创建文件夹
+backup_location=/home/mysqlBackup
+# 是否开启过期备份删除 ON为开启 OFF为关闭
+expire_backup_delete="ON"
+# 过期时间天数 默认为三天，此项只有在expire_backup_delete开启时有效
+expire_days=3
 
 # 本行开始以下不需要修改
-backup_time=`date +%Y%m%d%H%M`  #定义备份详细时间
-backup_Ymd=`date +%Y-%m-%d` #定义备份目录中的年月日时间
-backup_3ago=`date -d '3 days ago' +%Y-%m-%d` #3天之前的日期
-backup_dir=$backup_location/$backup_Ymd  #备份文件夹全路径
-welcome_msg="Welcome to use MySQL backup tools!" #欢迎语
+
+# 定义备份详细时间
+backup_time=`date +%Y%m%d%H%M`
+# 定义备份目录中的年月日时间
+backup_Ymd=`date +%Y-%m-%d`
+# 3天之前的日期
+backup_3ago=`date -d '3 days ago' +%Y-%m-%d`
+# 备份文件夹全路径
+backup_dir=$backup_location/$backup_Ymd
+# 欢迎语
+welcome_msg="Welcome to use MySQL backup tools!"
 
 # 判断MYSQL是否启动,mysql没有启动则备份退出
 mysql_ps=`ps -ef |grep mysql |wc -l`
