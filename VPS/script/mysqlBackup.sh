8971a46f4d199670cfaf90396427a2944a7bd23c
woytu <admin@woytu.com>
2019-02-20 17:27:31 +0800

    Update mysqlBackup.sh

--- VPS/script/mysqlBackup.sh
+++ VPS/script/mysqlBackup.sh
@@ -38,13 +38,13 @@ backup_3ago=`date -d '3 days ago' +%Y-%m-%d`
 #backup_dir=$backup_location/$backup_Ymd
 backup_dir=$backup_location
 # 欢迎语
-welcome_msg="Welcome to use MySQL backup tools!"
+welcome_msg="欢迎使用MySQL备份工具！"
 
 # 判断MYSQL是否启动,mysql没有启动则备份退出
 mysql_ps=`ps -ef |grep mysql |wc -l`
 mysql_listen=`netstat -an |grep LISTEN |grep $mysql_port|wc -l`
 if [ [$mysql_ps == 0] -o [$mysql_listen == 0] ]; then
-        echo "ERROR:MySQL is not running! backup stop!"
+        echo "错误：MySQL没有运行！备份停止！"
         exit
 else
         echo $welcome_msg
@@ -59,36 +59,35 @@ end
 
 flag=`echo $?`
 if [ $flag != "0" ]; then
-        echo "ERROR:Can't connect mysql server! backup stop!"
+        echo "错误：无法连接mysql服务器！备份停止！"
         exit
 else
-    echo "MySQL connect ok! Please wait......"
+    echo "MySQL连接成功! 请等待......"
     # 判断有没有定义备份的数据库，如果定义则开始备份，否则退出备份
     if [ "$backup_db_arr" != "" ];then
             #dbnames=$(cut -d ',' -f1-5 $backup_database)
             #echo "arr is (${backup_db_arr[@]})"
             for dbname in ${backup_db_arr[@]}
             do
-                    echo "database $dbname backup start..."
+                    echo "数据库 $dbname 备份开始..."
                     `mkdir -p $backup_dir`
                     `mysqldump -h$mysql_host -P$mysql_port -u$mysql_user -p$mysql_password $dbname --default-character-set=$mysql_charset | gzip > $backup_dir/$dbname-$backup_time.sql.gz`
                     flag=`echo $?`
                     if [ $flag == "0" ];then
-                            echo "database $dbname success backup to $backup_dir/$dbname-$backup_time.sql.gz"
+                            echo "数据库 $dbname 成功备份到 $backup_dir/$dbname-$backup_time.sql.gz"
                     else
-                            echo "database $dbname backup fail!"
+                            echo "数据库 $dbname 备份失败!"
                     fi
                     
             done
     else
-            echo "ERROR:No database to backup! backup stop"
+            echo "错误：没有要备份的数据库！备份停止"
             exit
     fi
     # 如果开启了删除过期备份，则进行删除操作
     if [ "$expire_backup_delete" == "ON" -a  "$backup_dir" != "" ];then
-             #`find $backup_dir/ -type d -o -type f -ctime +$expire_days -exec rm -rf {} \;`
-             `find $backup_dir/ -type d -mtime +$expire_days | xargs rm -rf`
-             echo "Expired backup data delete complete!"
+             `find $backup_dir/ -type f -mtime +$expire_days -print | xargs rm -f`
+             echo "删除备份文件成功!"
     fi
     endDate=`date +"%Y-%m-%d %H:%M:%S"`
     echo "[$endDate] 备份成功"
