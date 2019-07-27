817ce50976669d5b380b9d756cbad867214ac9b6
woytu <admin@woytu.com>
2019-07-27 14:57:35 +0800

    添加编译完成后自动提交到仓库命令

--- .vuepress/push.js
+++ .vuepress/push.js
@@ -4,7 +4,7 @@ const path = require('path');
 function execute(command) {
     // exec函数第一个参数是要执行的命令，第二个函数是配置选项，第三个参数是回调函数，
     // 第二个参数中一个比较常用的就是子进程的工作目录
-    // 在当前目录下的scripts文件夹里执行hexo g命令
+    // 在docs目录下执行命令
     exec(command, {cwd: path.resolve(__dirname, '../docs')}, (err, stdout, stderr) => {
         if (err) {
             console.log(err);
@@ -15,4 +15,7 @@ function execute(command) {
     });
 }
 
-execute("git init && git add -A && git commit -m 'deploy' && git push -f https://github.com/woytu/woytu.github.io.git master");
+// GitHub仓库地址
+let repo = "https://github.com/woytu/woytu.github.io.git";
+
+execute("git init && git add -A && git commit -m 'deploy' && git push -f " + repo + " master");
