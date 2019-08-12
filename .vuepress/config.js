c37d47fb732b68a109a97ce0bbf7fef515561255
bajins <admin@bajins.com>
2019-08-12 13:11:30 +0800

    移动logo位置

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -22,14 +22,14 @@ module.exports = {
     },
     // 额外的需要被注入到当前页面的 HTML <head> 中的标签，每个标签都可以以 [tagName, { attrName: attrValue }, innerHTML?] 的格式指定
     head: [
-        ['link', {rel: 'icon', href: `/icons/logo.png`}],
-        ['link', {rel: 'manifest', href: '/manifest.json'}],
+        ['link', {rel: 'icon', href: `/images/icons/logo.png`}],
+        ['link', {rel: 'manifest', href: '/images/manifest.json'}],
         ['meta', {name: 'theme-color', content: '#3eaf7c'}],
         ['meta', {name: 'apple-mobile-web-app-capable', content: 'yes'}],
         ['meta', {name: 'apple-mobile-web-app-status-bar-style', content: 'black'}],
-        ['link', {rel: 'apple-touch-icon', href: `/icons/apple-touch-icon-152x152.png`}],
-        ['link', {rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
-        ['meta', {name: 'msapplication-TileImage', content: '/icons/msapplication-icon-144x144.png'}],
+        ['link', {rel: 'apple-touch-icon', href: `/images/icons/apple-touch-icon-152x152.png`}],
+        ['link', {rel: 'mask-icon', href: '/images/icons/safari-pinned-tab.svg', color: '#3eaf7c'}],
+        ['meta', {name: 'msapplication-TileImage', content: '/images/icons/msapplication-icon-144x144.png'}],
         ['meta', {name: 'msapplication-TileColor', content: '#000000'}]
     ],
     theme: '',
