890755219ac4ef465e7f5e75fd72108b649148e6
woytu <admin@bajins.com>
2020-03-25 13:16:17 +0800

    update

--- .vuepress/nav.js
+++ .vuepress/nav.js
@@ -1,45 +1,40 @@
-module.exports = [
-    {
-        text: '更多资源',
-        items: [
-            {
-                // text: '',
-                items: [
-                    {
-                        text: 'WeChat小程序',
-                        link: 'https://www.woetu.com'
-                    },
-                    {
-                        text: '获取XShell',
-                        link: 'https://github.com/woytu/tool-gin'
-                    },
-                    {
-                        text: 'GoogleDrive',
-                        link: 'https://gd.woytu.workers.dev'
-                    },
-                    {
-                        text: 'OneDrive',
-                        link: 'https://yinkss-my.sharepoint.com/:f:/g/personal/claer_simplove_eu_org/EpzegyPPbiFPpOt5as1chqcBHW4a0jtvcIqOVzuJIY7-xg'
-                    },
-                ]
-            },
-            {
-                // text: 'Other',
-                items: [
-                    {
-                        text: '文件',
-                        link: '/files'
-                    },
-                    {
-                        text: 'Markdown工具',
-                        link: '/Other/Markdown#工具'
-                    },
-                    {
-                        text: 'Windows软件',
-                        link: '/Other/Windows软件'
-                    },
-                ]
-            }
-        ]
-    }
-]
+module.exports = [{
+    text: '更多资源',
+    items: [{
+            // text: '',
+            items: [{
+                    text: 'WeChat小程序',
+                    link: 'https://www.woetu.com'
+                },
+                {
+                    text: '获取XShell',
+                    link: 'https://github.com/woytu/tool-gin'
+                },
+                {
+                    text: 'GoogleDrive',
+                    link: 'https://gd.bajins.com'
+                },
+                {
+                    text: 'OneDrive',
+                    link: 'https://yinkss-my.sharepoint.com/:f:/g/personal/claer_simplove_eu_org/EpzegyPPbiFPpOt5as1chqcBHW4a0jtvcIqOVzuJIY7-xg'
+                },
+            ]
+        },
+        {
+            // text: 'Other',
+            items: [{
+                    text: '文件',
+                    link: '/files'
+                },
+                {
+                    text: 'Markdown工具',
+                    link: '/Other/Markdown#工具'
+                },
+                {
+                    text: 'Windows软件',
+                    link: '/Other/Windows软件'
+                },
+            ]
+        }
+    ]
+}]
