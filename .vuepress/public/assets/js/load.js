c181c7ed4ec476d67e1eba1bc4717b77fb92f1e4
woytu <admin@woytu.com>
2020-04-21 18:51:06 +0800

    update referrer

--- .vuepress/public/assets/js/load.js
+++ .vuepress/public/assets/js/load.js
@@ -10,6 +10,15 @@
     s.parentNode.insertBefore(bp, s);
 
     // 修改来源属性
-    const referrer = document.getElementById("referrer");
-    referrer.setAttribute("content", "never");
+    var referrer = document.querySelector("meta[name=referrer]");
+    if (referrer) {
+        console.log(referrer.getAttribute("content"));
+        referrer.setAttribute('content', 'never');
+        console.log(document.querySelector("meta[name=referrer]").getAttribute("content"));
+    } else {
+        referrer = document.createElement('meta');
+        referrer.name = "referrer";
+        referrer.content = "never";
+        document.getElementsByTagName('head')[0].appendChild(referrer);
+    }
 })();
